13abdd01114320af6312aa844ae97e847105405f
Fernando Anselmo <fernando.anselmo74@gmail.com>
2022-03-02 09:28:11 -0300

    Acertos do Hive

--- Apostilas/Hive/Sistemas/kafkaproj/src/main/java/kafkaproj/ConsumeMensagens.java
+++
@@ -1,8 +0,0 @@
-package kafkaproj;
-
-public class ConsumeMensagens {
-  public static void main(String[] args) {
-    ExConsumidor consumerThread = new ExConsumidor("testTopic");
-    consumerThread.start();
-  }
-}
