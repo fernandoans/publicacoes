e893e061aaed8291d64e28b53edcd9b0d9406c62
fernandoans <fernando.anselmo74@gmail.com>
2025-05-22 11:01:43 -0300

    Correção do CV

---
+++ Curriculo/geraHist/diagrama.py
@@ -0,0 +1,8 @@
+from diagrams import Cluster, Diagram
+from diagrams.aws.compute import ECS
+from diagrams.aws.database import ElastiCache, RDS
+from diagrams.aws.network import ELB
+from diagrams.aws.network import Route53
+
+with Diagram("Minha Carreira", show=False):
+    dns = Route53("Dez/2010")
